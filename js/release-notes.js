68a7ee25d67d149ea6980d5df92b3c31c4a4c69c
Michael van Antwerpen <michael.antwerpen@ewx.co.za>
2018-09-20 11:07:44 +0200

    Deployed c95940b with MkDocs version: 1.0.2

--- js/release-notes.js
+++ js/release-notes.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.rst-content').find('li').each(function() {
+  $('.section').find('li').each(function() {
     var text = this.textContent;
     text = text.replace(/CONSQL-\d+\s*/g, '')
     this.textContent = text;
